da7c5a59a5d71d804ad281dccf48228728712101
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2021-03-20 23:28:28 +0100

    Add driver code for peking express
    - Add main code for peking express, keep in mind this is still a work in progress and not completely finished
    - Add test files and template for using these files in unit tests

---
+++ test.py
@@ -0,0 +1,15 @@
+import peking_express
+import unittest
+import json
+import ast
+
+class TestSortFunction(unittest.TestCase):
+    def test_isSolvable(self):
+        fo = open("./test_files/test1.txt", encoding="utf8")
+        text = fo.read().split('\n')
+        fo.close()
+        result = peking_express.pekingExpress(json.loads(text[0]), int(text[1]), int(text[2]), ast.literal_eval(text[3]))
+        self.assertEqual(result, [3, 2, 2, 88, 88])
+
+if __name__ == '__main__':
+    unittest.main()
